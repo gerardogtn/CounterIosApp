a7cf6554f8d16db07cb622b6c11fab11fa27f04f
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-27 15:57:17 -0500

    :fire: Remove UIViews added programatically

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -11,51 +11,11 @@ import UIKit
 class ViewController: UIViewController {
   
   var count : Int = 0
-  var label : UILabel!
-  var isDarkBackground = true
+  @IBOutlet var label : UILabel!
+  var isLightBackground = true
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    setUpLabel()
-    setUpIncrementButton()
-    setUpDecrementButton()
-    setUpToggleBackgroundButton()
-  }
-  
-  func setUpLabel() {
-    let label = UILabel()
-    label.frame = CGRectMake(150, 150, 60, 60)
-    label.text = "0"
-    self.label = label
-    self.view.addSubview(label)
-  }
-  
-  func setUpIncrementButton() {
-    let button = UIButton()
-    button.frame = CGRectMake(100, 350, 90, 90)
-    button.setTitle("Inc", forState: .Normal)
-    button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-    
-    button.addTarget(self, action: #selector(ViewController.incrementCount), forControlEvents: UIControlEvents.TouchUpInside)
-    self.view.addSubview(button)
-  }
-  
-  func setUpDecrementButton() {
-    let button = UIButton()
-    button.frame = CGRectMake(200, 350, 90, 90)
-    button.setTitle("Dec", forState: .Normal)
-    button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-    button.addTarget(self, action: #selector(ViewController.decrementCount), forControlEvents: .TouchUpInside)
-    self.view.addSubview(button)
-  }
-  
-  func setUpToggleBackgroundButton() {
-    let button = UIButton()
-    button.frame = CGRectMake(100, 450, 200, 200)
-    button.setTitle("Toggle background", forState: .Normal)
-    button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-    button.addTarget(self, action: #selector(ViewController.toggleBackground), forControlEvents: .TouchUpInside)
-    self.view.addSubview(button)
   }
 
   override func didReceiveMemoryWarning() {
@@ -74,12 +34,12 @@ class ViewController: UIViewController {
   }
 
   func toggleBackground() {
-    if self.isDarkBackground {
+    if !self.isLightBackground {
       self.view.backgroundColor = UIColor.redColor()
     } else {
       self.view.backgroundColor = UIColor.whiteColor()
     }
-    self.isDarkBackground = !self.isDarkBackground
+    self.isLightBackground = !self.isLightBackground
   
   }
   
