fd73d62438be7f3add72734bed511b89c9d7acac
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-27 15:39:19 -0500

    :rocket: Add target to button

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,31 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var count : Int = 0
+  var label : UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     setUpLabel()
-    setUpButton()
+    setUpIncrementButton()
   }
   
   func setUpLabel() {
     let label = UILabel()
     label.frame = CGRectMake(150, 150, 60, 60)
     label.text = "0"
+    self.label = label
     self.view.addSubview(label)
   }
   
-  func setUpButton() {
+  func setUpIncrementButton() {
     let button = UIButton()
     button.frame = CGRectMake(150, 350, 90, 90)
     button.setTitle("Count", forState: UIControlState.Normal)
     button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+    
+    button.addTarget(self, action: #selector(ViewController.incrementCount), forControlEvents: UIControlEvents.TouchUpInside)
     self.view.addSubview(button)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func incrementCount() {
+    self.count += 1
+    self.label.text = "\(self.count)"
   }
 
 
